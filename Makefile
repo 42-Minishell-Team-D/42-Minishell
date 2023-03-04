4190fc9143de8a4ce0a771c24d55d7fb232b0458
Diogo Dantas goncalves <ddantas-@c2r9s8.42lisboa.com>
2023-03-04 10:42:06 +0000

    Init commit :D

---
+++ Makefile
@@ -0,0 +1,41 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/04 09:52:07 by ddantas-          #+#    #+#              #
+#    Updated: 2023/03/04 10:19:07 by ddantas-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = minishell
+
+CFLAGS = -g -pthread -Wall -Wextra -Werror
+RM = rm
+LIBFT = ./libs/libft/libft.a
+FILESC = ./src/main.c
+
+OBJS = $(FILESC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(OBJS)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIBFT)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(LIBFT):
+	$(MAKE) -C ./libs/libft
+
+clean:
+	$(RM) -f $(OBJS)
+	
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re bonus
---
+++ libs/minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 09:54:01 by ddantas-          #+#    #+#             */
+/*   Updated: 2023/03/04 10:40:39 by ddantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# define _XOPEN_SOURCE_EXTENDED 1
+# define _POSIX_SOURCE
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <curses.h>
+# include <term.h>
+# include <termios.h>
+# include <unistd.h>
+# include <string.h>
+# include <dirent.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <sys/ioctl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// missing: ttyslot, sigaddset, lstat
+
+/*			src/main.c 			*/
+
+
+#endif
