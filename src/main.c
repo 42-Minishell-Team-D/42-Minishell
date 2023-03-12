afc3ab0d80101a1ed6d26b0ca288a9039d9954ad
loris <loris.penelon@outlook.com>
2023-03-12 13:15:19 +0000

    select_arg norm

--- src/main.c
+++ src/main.c
@@ -17,22 +17,29 @@ static void handler(int sig, siginfo_t *id, void *content)
 	return ;
 }
 
-int	select_arg(char *prompt, int *rt)
+int select_arg(char *p, int *rt)
 {
-	if (prompt[0] == 'p' && prompt[1] == 'w' && prompt[2] == 'd' && prompt[3] == '\0')
+	if (p[0] == 'e' && p[1] == 'c' && p[2] == 'h' && p[3] == 'o')
+		printf("exec_echo\n");
+	else if (p[0] == 'c' && p[1] == 'd')
+		*rt = exec_cd(p);
+	else if (pad_char[0] == 'p' && p[1] == 'w' && p[2] == 'd' && p[3] == '\0')
 		*rt = exec_pwd();
-	else if (prompt[0] == 'e' && prompt[1] == 'n' && prompt[2] == 'v' && prompt[3] == '\0')
+	else if (p[0] == 'e' && p[1] == 'x' && p[2] == 'p' && p[3] == 'o' && p[4] == 'r' && p[5] == 't' && p[6] == '\0')
+		printf("exec_export\n");
+	else if (p[0] == 'u' && p[1] == 'n' && p[2] == 's' && p[3] == 'e' && p[4] == 't' && p[5] == '\0')
+		printf("exec_unset\n");
+	else if (p[0] == 'e' && p[1] == 'n' && p[2] == 'v' && p[3] == '\0')
 		*rt = exec_env();
-	else if (prompt[0] == 'c' && prompt[1] == 'd')
-		*rt = exec_cd(prompt);
-	else if (prompt[0] == 'e' && prompt[1] == 'x' && prompt[2] == 'i' && prompt[3] == 't' && prompt[4] == '\0')
-		exit(0);	// That causes leak, even if free(prompt); is implemented
-	else if (prompt[0] == '.' && prompt[1] == '/')
-		*rt = exec_prog(prompt);
+	else if (p[0] == 'e' && p[1] == 'x' && p[2] == 'i' && p[3] == 't' && p[4] == '\0')
+		exit(0); // That causes leak, even if free(prompt); is implemented
+	else if (p[0] == '.' && p[1] == '/')
+		*rt = exec_prog(p);
 	else
-		printf("minishell: %s command not found :\\\n", prompt);
+		printf("minishell: %s command not found :\\\n", p);
 	return (0);
 }
+
 
 int	main(void)
 {
