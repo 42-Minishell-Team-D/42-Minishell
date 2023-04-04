028703e871e371238d5c3005e36e510ce939198a
Loris pierre jean-francois Penelon <lpenelon@c2r3s6.42lisboa.com>
2023-04-04 17:15:25 +0100

    token validity

--- src/parser/check_tokens_validity.c
+++ src/parser/check_tokens_validity.c
@@ -1,29 +1,32 @@
 #include "../../libs/minishell.h"
 
-int valid_odd_token(char *token)
+int	valid_odd_token(char *token)
 {
-	char	*valid_odd_inputs[] = {"|", ">", "<", ">>", "<<", NULL};
-    int     i;
-    
-    i = 0;
-    while (ft_strncmp(token, valid_odd_inputs[i], 100) != 0)
-        i++;
-    if (valid_odd_inputs[i] == NULL)
-        return (1);
-    return (0);
+	int		i;
+	char	**valid_odd_inputs;
+
+	i = 0;
+	valid_odd_inputs = {"|", ">", "<", ">>", "<<", NULL};
+	while (ft_strncmp(token, valid_odd_inputs[i], 3) != 0)
+		i++;
+	if (valid_odd_inputs[i] == NULL)
+		return (1);
+	return (0);
 }
 
-int valid_even_token(char *token)
+int	valid_even_token(char *token)
 {
-	char	*valid_even_inputs[] = {"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL};
-    int     i;
-    
-    i = 0;
-    while (ft_strncmp(token, valid_even_inputs[i], 100) != 0)
-        i++;
-    if (valid_even_inputs[i] == NULL)
-        return (1);
-    return (0);
+	int		i;
+	char	**valid_even_inputs;
+
+	i = 0;
+	valid_even_inputs = {"echo\0", "echo ", "cd\0", "cd " "pwd\0", "pwd ",\
+	"export\0", "export ","unset\0", "unset ", "env\0", "env ", "exit\0", "exit ", NULL};
+	while (ft_strncmp(token, valid_even_inputs[i], 100) != 0)
+		i++;
+	if (valid_even_inputs[i] == NULL)
+		return (1);
+	return (0);
 }
 
 int	check_invalid_inputs(char **tokens)
