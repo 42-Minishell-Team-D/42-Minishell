d444b798fbd15f0e709ecc8ff4c4658a3594396c
Loris pierre jean-francois Penelon <lpenelon@c2r9s7.42lisboa.com>
2023-04-06 14:33:12 +0100

    check_syntax done

--- src/main.c
+++ src/main.c
@@ -92,8 +92,8 @@ int	main(void)
 			// print_tokens(data.tokens);
 			data.tree = create_tree(data.tokens, data.tree);
 			// print_tree(data.tree);
-			if (check_syntax(data.tree) != 0)		// not acting like the shell yet
-				executor(&data);
+			// if (check_syntax(data.tree) != 0)		// not acting exactly like the shell
+				// executor(&data);
 			free_after_execution(&data);
 		}
 		// if (data.prompt != NULL)
