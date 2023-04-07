e869f4d368c73750c29834d9bb1c25afbbce7bae
Diogo Dantas goncalves <ddantas-@c2r9s7.42lisboa.com>
2023-04-07 11:52:16 +0100

    comment exec

--- src/main.c
+++ src/main.c
@@ -1,30 +1,5 @@
 #include "../libs/minishell.h"
 
-int select_arg(char *p, t_data *data)
-{
-	if (p[0] == 'e' && p[1] == 'c' && p[2] == 'h' && p[3] == 'o')
-		data->rt = exec_echo(p + 5, 0, data);
-	else if (p[0] == 'c' && p[1] == 'd')
-		data->rt = exec_cd(p);
-	else if (p[0] == 'p' && p[1] == 'w' && p[2] == 'd' && p[3] == '\0')
-		data->rt = exec_pwd();
-	else if (p[0] == 'e' && p[1] == 'x' && p[2] == 'p' && p[3] == 'o' && p[4] == 'r' && p[5] == 't')
-		data->rt = exec_export(data, p);
-	else if (p[0] == 'u' && p[1] == 'n' && p[2] == 's' && p[3] == 'e' && p[4] == 't')
-		data->rt = exec_unset(data, p);
-	else if (p[0] == 'e' && p[1] == 'n' && p[2] == 'v' && p[3] == '\0')
-		data->rt = exec_env(data);
-	else if (p[0] == 'e' && p[1] == 'x' && p[2] == 'i' && p[3] == 't')
-		exec_exit(p);
-	else if ((p[0] == '.' && p[1] == '/') || (p[0] == '.' && p[1] == '.' && p[2] == '/'))
-		data->rt = exec_prog(p);
-	else if (p[0] == 0)	// enter is giving new prompt
-		return (0);
-	else
-		printf("minishell: %s command not found \n", p);
-	return (0);
-}
-
 void print_tokens(char **tokens)
 {
 	printf("Tokens:\n");
@@ -96,8 +71,6 @@ int	main(void)
 				executor(&data);
 			free_after_execution(&data);
 		}
-		// if (data.prompt != NULL)
-		// 	select_arg(data.prompt, &data);
 	}
 	rl_clear_history();
 	free_at_exit(&data);
--- src/parser/check_syntax.c
+++ src/parser/check_syntax.c
@@ -1,12 +1,3 @@
-// for example
-// | ok > ok
-// this doesn't create a ok file
-// unlike 
-// ok | ok > ok
-
-// My understanding: Syntax is checked before executor runs, errors stop the process. and ouput "minishell: syntax error near unexpected token `|'"
-// If no syntax errors, executor run every command and ouput "ok: command not found".
-
 #include "../../libs/minishell.h"
 
 int	invalid_odd_token(char *token)
