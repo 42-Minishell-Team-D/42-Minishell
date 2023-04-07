d369006b8bc3f07895a4518eadd1c06691e59d66
Diogo Dantas goncalves <ddantas-@c2r9s7.42lisboa.com>
2023-04-07 16:06:39 +0100

    more commented stuff :D

--- src/executor/executor_v1.c
+++ src/executor/executor_v1.c
@@ -27,3 +27,30 @@
 	printf("Parent Process: '%s'\n", read_something);
 	return (0);
 }*/
+
+/*
+Pipe example
+int main(int argc, char *argv[])
+{
+	int pipes[2][2];
+
+	pipe(pipes[0]);
+	pipe(pipes[1]);
+
+	pid_t pid = fork();
+	if (pid == 0) {
+		// Create a child executing the command "echo hi"
+		dup2(pipes[1][1], 1);	// Redirect stdout to pipe
+		dup2(pipes[0][0], 0); 	// Redirect stdin to pipe
+		char	*args[] = {"/bin/echo",  "hi", NULL}; // Arguments to execve
+		execve("/bin/echo", args, NULL); // Execute the command
+		return (0); // This line should never be reached
+	}
+	wait(NULL); // Wait for the child to finish
+	char *buf = malloc(1024); // Allocate a buffer
+	read(pipes[1][0], buf, 1024); // Read the output of the child
+	write(1, buf, strlen(buf)); // Print the output of the child
+
+	return (0);
+}
+*/
