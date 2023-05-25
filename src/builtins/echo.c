d15e55a2c4ed47baea0ba27eb268b4150e6af9c9
Loris pierre jean-francois Penelon <lpenelon@c1r11s4.42lisboa.com>
2023-05-25 18:41:05 +0100

    echo need to add newline

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,30 +1,5 @@
 #include "../../libs/minishell.h"
 
-// static char *get_env(char *str, char **env)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*tmp;
-
-// 	i = 0;
-// 	j = 0;
-// 	while (env[i] != NULL)
-// 	{
-// 		printf("ft_strncmp(str, env[i], get_biggest_len(str, env[i])) = %i\n", ft_strncmp(str, env[i], get_biggest_len(str, env[i])));
-// 		if (ft_strncmp(str, env[i], get_biggest_len(str, env[i])) == 0)
-// 		{
-// 			tmp = ft_strdup(env[i]);
-// 			while (tmp[j] != '=')
-// 				j++;
-// 			ft_printf_fd(1, "%s", str + j + 1);
-// 			return ("\0");
-// 		}
-// 		i++;
-// 	}
-// 	ft_printf_fd(1, "that's a test\n");
-// 	return ("\0");
-// }
-
 int	exec_echo(char **split, t_data *data)
 {
 	int		i;
@@ -39,8 +14,9 @@ int	exec_echo(char **split, t_data *data)
 	{
 		i = 0;
 		str = split[n];
-		if (str[i] == '$')
-			str = get_env(str + 1, data->env);
+		if (str[i] == '$' && str[i + 1] != '\0')
+			ft_printf_fd(1, "\n");
+			
 		while (str[i] != '\0')
 		{
 			ft_printf_fd(1, "%c", str[i]);
