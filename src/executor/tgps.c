ff2816fe41e4108c11ab74c0513fab270bb111e1
Diogo Dantas goncalves <ddantas-@c1r18s7.42lisboa.com>
2023-05-26 17:15:04 +0100

    init pipes fd to -1 in order not to confund with STDIN

--- src/executor/tgps.c
+++ src/executor/tgps.c
@@ -9,8 +9,8 @@ static void	init_pipes(t_data *data)
 	while (i < get_number_of_processes(data->tree) + 1)
 	{
 		data->pipes[i] = (int *)ft_calloc(2, sizeof(int));
-		data->pipes[i][0] = 0;
-		data->pipes[i][1] = 0;
+		data->pipes[i][0] = -1;
+		data->pipes[i][1] = -1;
 		i++;
 	}
 }
@@ -23,10 +23,12 @@ int	init_executor(t_data *data)
 	data->pids = malloc(sizeof(int) * (get_number_of_processes(data->tree)));
 	init_pipes(data);
 	while (i < get_number_of_processes(data->tree) + 1)
+	{
 		if (pipe(data->pipes[i++]) < 0)
 		{
 			close_free_pipes_pids(data);
 			return (1);
 		}
+	}
 	return (0);
 }
