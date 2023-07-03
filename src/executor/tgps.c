31301a5bb082faf63dfbd1806150b018ddf1a28f
Loris pierre jean-francois Penelon <lpenelon@c1r14s8.42lisboa.com>
2023-07-03 12:33:50 +0100

    fix diogo

--- src/executor/tgps.c
+++ src/executor/tgps.c
@@ -5,6 +5,7 @@ static void	init_pipes(t_data *data)
 	int	i;
 
 	i = 0;
+	data->pipes = NULL;
 	if (get_number_of_processes(data->tree) > 1)
 		data->pipes = (int **)ft_calloc(sizeof(int *), (get_number_of_processes(data->tree) - 1));
 	while (i < get_number_of_processes(data->tree) - 1)
