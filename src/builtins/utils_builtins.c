bc4e15eab889a662c310ff74569338227b78af37
Loris pierre jean-francois Penelon <lpenelon@c1r14s8.42lisboa.com>
2023-07-03 14:38:12 +0100

    utils builtins

--- src/builtins/utils_builtins.c
+++ src/builtins/utils_builtins.c
@@ -12,18 +12,38 @@ char	*get_before_equal_sign(char *var)
 
 char	*get_before_equal_sign_export(char *var)
 {
-	char	*join;
-
-	join = get_before_equal_sign(var);
-	free(var);
-	var = ft_strjoin(join, "\0");
-	free(join);
+	var = get_before_equal_sign(var);
+	// remove "declare -x " from the variable
 	if (ft_strncmp(var, "declare -x ", 11) == 0)
-	{
-		join = ft_substr(var, 11, ft_strlen(var));
-		free(var);
-		var = ft_strjoin(join, "\0");
-		free(join);
-	}
+		var = ft_substr(var, 11, ft_strlen(var));
 	return (var);
 }
+
+
+// char	*get_before_equal_sign(char *var)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (var[i] != '\0' && var[i] != '=')
+// 		i++;
+// 	return (ft_substr(var, 0, i));
+// }
+
+// char	*get_before_equal_sign_export(char *var)
+// {
+// 	char	*join;
+
+// 	join = get_before_equal_sign(var);
+// 	free(var);
+// 	var = ft_strjoin(join, "\0");
+// 	free(join);
+// 	if (ft_strncmp(var, "declare -x ", 11) == 0)
+// 	{
+// 		join = ft_substr(var, 11, ft_strlen(var));
+// 		free(var);
+// 		var = ft_strjoin(join, "\0");
+// 		free(join);
+// 	}
+// 	return (var);
+// }
