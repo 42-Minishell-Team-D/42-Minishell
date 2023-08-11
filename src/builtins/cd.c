d4d38e049a2ccd1ceda27c35039633bdea94e132
Loris pierre jean-francois Penelon <lpenelon@c2r6s3.42lisboa.com>
2023-08-11 10:42:45 +0100

    norm CD PWD

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: lpenelon <lpenelon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 22:09:59 by loris             #+#    #+#             */
-/*   Updated: 2023/08/11 10:32:41 by lpenelon         ###   ########.fr       */
+/*   Updated: 2023/08/11 10:41:05 by lpenelon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,16 @@ static int	exists_perms(const char *path, int option)
 	return (0);
 }
 
-static void	update_pwd(t_data *data)
+static void	update_pwd(t_data *data, int n)
 {
-	int		n;
 	char	cwd[PATH_MAX];
 
-	n = 0;
 	getcwd(cwd, sizeof(cwd));
 	while (data->env[n] != NULL)
 	{
 		if (ft_strncmp(data->env[n], "PWD=", 4) == 0)
 		{
 			free(data->env[n]);
-			data->env[n] = NULL;
 			data->env[n] = ft_strjoin("PWD=", cwd);
 			break ;
 		}
@@ -47,7 +44,6 @@ static void	update_pwd(t_data *data)
 		if (ft_strncmp(&data->export[n][11], "PWD=", 4) == 0)
 		{
 			free(data->export[n]);
-			data->export[n] = NULL;
 			data->join = ft_strjoin("declare -x PWD=\"", cwd);
 			data->export[n] = ft_strjoin(data->join, "\"\0");
 			free(data->join);
@@ -56,19 +52,16 @@ static void	update_pwd(t_data *data)
 	}
 }
 
-static void	update_oldpwd(t_data *data)
+static void	update_oldpwd(t_data *data, int n)
 {
-	int		n;
 	char	cwd[PATH_MAX];
 
-	n = 0;
 	getcwd(cwd, sizeof(cwd));
 	while (data->env[n] != NULL)
 	{
 		if (ft_strncmp(data->env[n], "OLDPWD=", 7) == 0)
 		{
 			free(data->env[n]);
-			data->env[n] = NULL;
 			data->env[n] = ft_strjoin("OLDPWD=", cwd);
 			break ;
 		}
@@ -80,7 +73,6 @@ static void	update_oldpwd(t_data *data)
 		if (ft_strncmp(&data->export[n][11], "OLDPWD=", 7) == 0)
 		{
 			free(data->export[n]);
-			data->export[n] = NULL;
 			data->join = ft_strjoin("declare -x OLDPWD=\"", cwd);
 			data->export[n] = ft_strjoin(data->join, "\"\0");
 			free(data->join);
@@ -100,9 +92,9 @@ static int	if_split_null(t_data *data)
 		ft_printf_fd(2, "minishell: cd: HOME not set\n");
 		return (1);
 	}
-	update_oldpwd(data);
+	update_oldpwd(data, 0);
 	chdir(home);
-	update_pwd(data);
+	update_pwd(data, 0);
 	free(home);
 	return (0);
 }
@@ -125,8 +117,8 @@ int	exec_cd(char **split, t_data *data)
 %s: Permission denied\n", split[1]);
 		return (1);
 	}
-	update_oldpwd(data);
+	update_oldpwd(data, 0);
 	chdir(split[1]);
-	update_pwd(data);
+	update_pwd(data, 0);
 	return (0);
 }
